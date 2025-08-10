fbd973c6878cd5ee6ad42739a606616253016552
abhimanyu-byte <agarwal.abhimanyu9801@gmail.com>
2025-08-10 09:19:26 -0400

    App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="title">💬 Contact Manager</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
